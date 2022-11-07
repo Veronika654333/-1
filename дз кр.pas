5afd3504c4436121fb7059d405c8db71b60b27a1
Veronika654333 <117628730+Veronika654333@users.noreply.github.com>
2022-11-07 10:36:09 +0300

    Add files via upload

---
+++ дз кр.pas
@@ -0,0 +1,13 @@
+﻿program funktia;
+var x: real;
+begin
+   x:=-10;
+while x<=4 do 
+  begin
+  if x <-8 then writeln ('y = ',sqr(x)/ sin(x)-tan(x)/ power(e,x):1:1) else 
+    if x < -1 then writeln ('y = ', tan(x):1:1) else
+      if x < 2 then writeln ('y = ',sin(x)/4 - power(x, 0.1*x)/ power (e,x):1:1) else 
+        writeln ('y = ',cos(2*x):1:1);
+            x:=x+0.2;
+      end;
+end.
